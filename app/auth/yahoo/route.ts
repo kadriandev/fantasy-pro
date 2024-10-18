ab2c724826a09433b4605824cac520415cf7df25
kadriandev <kyle.monteiro@gmail.com>
2024-10-17 22:44:13 -0400

    Created stripe integration

--- app/auth/yahoo/route.ts
+++
@@ -1,7 +0,0 @@
-import { authToken } from "@/lib/yahoo/auth";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-	const redirect_uri = await authToken();
-	return NextResponse.redirect(redirect_uri);
-}
