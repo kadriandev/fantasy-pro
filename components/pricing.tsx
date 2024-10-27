abb4628480ef0f476711bf7498705528472da461
kadriandev <kyle.monteiro@gmail.com>
2024-10-27 15:22:53 -0400

    Modified pricing page

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -64,7 +64,11 @@ export default function Pricing({ user, products, subscription }: Props) {
 		stripe?.redirectToCheckout({ sessionId });
 	};
 
-	if (!products.length) {
+	const product = products.find(
+		(p) => p.id === process.env.NEXT_PUBLIC_STRIPE_PRODUCT_ID,
+	);
+
+	if (!product) {
 		return (
 			<section className="bg-black">
 				<div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
@@ -84,59 +88,60 @@ export default function Pricing({ user, products, subscription }: Props) {
 				</div>
 			</section>
 		);
-	} else {
-		return (
-			<section className="bg-black">
-				<div className="mt-12 space-y-0 sm:mt-16 flex flex-wrap justify-center gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0">
-					{products.map((product, i) => {
-						const price = product?.prices[0];
-						if (!price) return null;
-						const priceString = new Intl.NumberFormat("en-US", {
-							style: "currency",
-							currency: price.currency!,
-							minimumFractionDigits: 0,
-						}).format((price?.unit_amount || 0) / 100);
-
-						return (
-							<div
-								key={i}
-								className={cn(
-									"flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900",
-									{
-										"border border-pink-500": subscription
-											? product.name === subscription?.prices?.products?.name
-											: product.name === "Freelancer",
-									},
-									"flex-1", // This makes the flex item grow to fill the space
-									"basis-1/3", // Assuming you want each card to take up roughly a third of the container's width
-									"max-w-xs", // Sets a maximum width to the cards to prevent them from getting too large
-								)}
-							>
-								<div className="p-6">
-									<h2 className="text-2xl font-semibold leading-6 text-white">
-										{product.name}
-									</h2>
-									<p className="mt-4 text-zinc-300">{product.description}</p>
-									<p className="mt-8">
-										<span className="text-5xl font-extrabold white">
-											{priceString}
-										</span>
-										<span className="text-base font-medium text-zinc-100">
-											/{price.interval}
-										</span>
-									</p>
-									<Button
-										onClick={() => handleStripeCheckout(price)}
-										className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
-									>
-										{subscription ? "Manage" : "Subscribe"}
-									</Button>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-			</section>
-		);
 	}
+
+	return (
+		<section className="bg-black">
+			<h1 className="text-4xl font-bold">{product.name}</h1>
+			<h2 className="text-xl text-zinc-600">{product.description}</h2>
+			<div className="mt-12 space-y-0 sm:mt-16 flex flex-wrap justify-center gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0">
+				{product.prices.map((price, i) => {
+					const priceString = new Intl.NumberFormat("en-US", {
+						style: "currency",
+						currency: price.currency!,
+						minimumFractionDigits: 0,
+					}).format((price?.unit_amount || 0) / 100);
+
+					return (
+						<div
+							key={i}
+							className={cn(
+								"flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900",
+								{
+									"border border-pink-500": subscription
+										? product.name === subscription?.prices?.products?.name
+										: product.name === "Freelancer",
+								},
+								"flex-1", // This makes the flex item grow to fill the space
+								"basis-1/3", // Assuming you want each card to take up roughly a third of the container's width
+								"max-w-xs", // Sets a maximum width to the cards to prevent them from getting too large
+							)}
+						>
+							<div className="p-6">
+								<h2 className="text-2xl font-semibold leading-6 text-white">
+									{price.interval === "month" && "Monthly"}
+									{price.interval === "year" && "Annual"}
+								</h2>
+								<p className="mt-4 text-zinc-300">{price.description}</p>
+								<p className="mt-8">
+									<span className="text-5xl font-extrabold white">
+										{priceString}
+									</span>
+									<span className="text-base font-medium text-zinc-100">
+										/{price.interval}
+									</span>
+								</p>
+								<Button
+									onClick={() => handleStripeCheckout(price)}
+									className="block w-full py-2 mt-8 text-sm font-semibold text-center rounded-md "
+								>
+									{subscription ? "Manage" : "Subscribe"}
+								</Button>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
