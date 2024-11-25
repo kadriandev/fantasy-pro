cf4a62041c4146c8374cfb2ec3fa57c912ba75f7
kadriandev <kyle.monteiro@gmail.com>
2024-11-25 16:32:04 -0500

    Removed smtp message

--- app/(public)/sign-up/page.tsx
+++ app/(public)/sign-up/page.tsx
@@ -4,57 +4,55 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { SmtpMessage } from "../smtp-message";
 
 export default function Signup({ searchParams }: { searchParams: Message }) {
-	if ("message" in searchParams) {
-		return (
-			<div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
-				<FormMessage message={searchParams} />
-			</div>
-		);
-	}
+  if ("message" in searchParams) {
+    return (
+      <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
+        <FormMessage message={searchParams} />
+      </div>
+    );
+  }
 
-	return (
-		<>
-			<form className="flex flex-col min-w-64 max-w-64 mx-auto">
-				<h1 className="text-2xl font-medium">Sign up</h1>
-				<p className="text-sm text text-foreground">
-					Already have an account?{" "}
-					<Link className="text-primary font-medium underline" href="/sign-in">
-						Sign in
-					</Link>
-				</p>
-				<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-					<Label htmlFor="first_name">First Name</Label>
-					<Input name="first_name" placeholder="John" required />
-					<Label htmlFor="last_name">Last Name</Label>
-					<Input name="last_name" placeholder="Smith" required />
-					<Label htmlFor="email">Email</Label>
-					<Input name="email" placeholder="you@example.com" required />
-					<Label htmlFor="password">Password</Label>
-					<Input
-						type="password"
-						name="password"
-						placeholder="Your password"
-						minLength={6}
-						required
-					/>
-					<Label htmlFor="confirm_password">Confirm Password</Label>
-					<Input
-						type="password"
-						name="confirm_password"
-						placeholder="Re-type password"
-						minLength={6}
-						required
-					/>
-					<SubmitButton formAction={signUpAction} pendingText="Signing up...">
-						Sign up
-					</SubmitButton>
-					<FormMessage message={searchParams} />
-				</div>
-			</form>
-			<SmtpMessage />
-		</>
-	);
+  return (
+    <>
+      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
+        <h1 className="text-2xl font-medium">Sign up</h1>
+        <p className="text-sm text text-foreground">
+          Already have an account?{" "}
+          <Link className="text-primary font-medium underline" href="/sign-in">
+            Sign in
+          </Link>
+        </p>
+        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+          <Label htmlFor="first_name">First Name</Label>
+          <Input name="first_name" placeholder="John" required />
+          <Label htmlFor="last_name">Last Name</Label>
+          <Input name="last_name" placeholder="Smith" required />
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" placeholder="you@example.com" required />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            minLength={6}
+            required
+          />
+          <Label htmlFor="confirm_password">Confirm Password</Label>
+          <Input
+            type="password"
+            name="confirm_password"
+            placeholder="Re-type password"
+            minLength={6}
+            required
+          />
+          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
+            Sign up
+          </SubmitButton>
+          <FormMessage message={searchParams} />
+        </div>
+      </form>
+    </>
+  );
 }
