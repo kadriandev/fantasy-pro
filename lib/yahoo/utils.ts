6fccf4dd955e9accdd24642450be380ced8bb283
kadriandev <kyle.monteiro@gmail.com>
2024-12-08 19:53:17 -0500

    Formatted

--- lib/yahoo/utils.ts
+++ lib/yahoo/utils.ts
@@ -3,72 +3,72 @@ import { createYahooClient } from ".";
 import { DBFantasyStats, FantasyStats, YahooLeagueScoreboard } from "./types";
 
 export const getStatMapper = async (
-	supabase: SupabaseClient,
-	league_key: string,
+  supabase: SupabaseClient,
+  league_key: string,
 ): Promise<Map<string, { name: string; abbr: string }>> => {
-	const statMapper = new Map<string, { name: string; abbr: string }>();
-	const { data: stat_categories } = await supabase
-		.from("leagues")
-		.select("stat_categories")
-		.eq("league_key", league_key)
-		.maybeSingle();
-	if (!stat_categories?.stat_categories) return statMapper;
+  const statMapper = new Map<string, { name: string; abbr: string }>();
+  const { data: stat_categories } = await supabase
+    .from("leagues")
+    .select("stat_categories")
+    .eq("league_key", league_key)
+    .maybeSingle();
+  if (!stat_categories?.stat_categories) return statMapper;
 
-	for (let stat of stat_categories.stat_categories) {
-		statMapper.set(stat.stat_id + "", { name: stat.name, abbr: stat.abbr });
-	}
+  for (let stat of stat_categories.stat_categories) {
+    statMapper.set(stat.stat_id + "", { name: stat.name, abbr: stat.abbr });
+  }
 
-	return statMapper;
+  return statMapper;
 };
 
 export const getCurrentWeekStats = async (
-	league_key: string,
+  league_key: string,
 ): Promise<Array<FantasyStats>> => {
-	const yf = await createYahooClient();
+  const yf = await createYahooClient();
 
-	const scoreboard: YahooLeagueScoreboard =
-		await yf.league.scoreboard(league_key);
+  const scoreboard: YahooLeagueScoreboard =
+    await yf.league.scoreboard(league_key);
 
-	return scoreboard.scoreboard.matchups
-		.reduce((acc: any[], curr: any) => {
-			acc.push(curr.teams);
-			return acc;
-		}, [])
-		.flat()
-		.map((s) => {
-			const res: FantasyStats = {
-				team_id: s.team_id,
-				team: s.name,
-			};
-			s.stats.forEach((r: any) => {
-				res[r.stat_id] = r.value;
-			});
-			return res;
-		});
+  return scoreboard.scoreboard.matchups
+    .reduce((acc: any[], curr: any) => {
+      acc.push(curr.teams);
+      return acc;
+    }, [])
+    .flat()
+    .map((s) => {
+      const res: FantasyStats = {
+        team_id: s.team_id,
+        team: s.name,
+      };
+      s.stats.forEach((r: any) => {
+        res[r.stat_id] = r.value;
+      });
+      return res;
+    });
 };
 
 export const getMatchupTeamId = async (league_key: string, team_id: string) => {
-	const yf = createYahooClient();
-	const scoreboard = (await yf.league.scoreboard(
-		league_key,
-	)) as YahooLeagueScoreboard;
+  const yf = createYahooClient();
+  const scoreboard = (await yf.league.scoreboard(
+    league_key,
+  )) as YahooLeagueScoreboard;
 
-	const opponent_team_key = scoreboard.scoreboard.matchups
-		.find(
-			(m) => m.teams[0].team_id === team_id || m.teams[1].team_id === team_id,
-		)
-		?.teams.find((t) => t.team_id != team_id)?.team_id;
-	return opponent_team_key;
+  const opponent_team_key = scoreboard.scoreboard.matchups
+    .find(
+      (m) => m.teams[0].team_id === team_id || m.teams[1].team_id === team_id,
+    )
+    ?.teams.find((t) => t.team_id != team_id)?.team_id;
+  return opponent_team_key;
 };
 
 export const groupStatsByWeek = (
-	data: Array<DBFantasyStats>,
+  data: Array<DBFantasyStats>,
 ): Array<Array<DBFantasyStats>> => {
-	return data
-		.reduce((acc: Array<Array<DBFantasyStats>>, curr) => {
-			if (!acc[curr.week - 1]) acc[curr.week - 1] = [];
-			acc[curr.week - 1].push(curr);
-			return acc;
-		}, [])
-		.reverse();
+  return data
+    .reduce((acc: Array<Array<DBFantasyStats>>, curr) => {
+      if (!acc[curr.week - 1]) acc[curr.week - 1] = [];
+      acc[curr.week - 1].push(curr);
+      return acc;
+    }, [])
+    .reverse();
 };
