a5d21ba491297a3c2a9537c658ee88c1cc83c140
kadriandev <kyle.monteiro@gmail.com>
2025-01-23 14:44:41 -0500

    fasdf

--- app/auth/yahoo/callback/route.ts
+++ app/auth/yahoo/callback/route.ts
@@ -6,28 +6,29 @@ import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
   const cookieStore = cookies();
-  const res = await accessToken(request);
-
-  if (res.error) {
-    cookieStore.delete("access_token");
-    cookieStore.delete("refresh_token");
-    return NextResponse.redirect(getURL("/"));
-  }
-
-  if (res.access_token?.length)
-    cookieStore.set("access_token", res.access_token as string, {
-      expires: new Date(Date.now() + res.expires_in * 1000),
-      httpOnly: true,
-      path: "/",
-    });
-
-  if (res.refresh_token?.length)
-    cookieStore.set("refresh_token", res.refresh_token as string, {
-      httpOnly: true,
-      path: "/",
-    });
 
   try {
+    const res = await accessToken(request);
+
+    if (res.error) {
+      cookieStore.delete("access_token");
+      cookieStore.delete("refresh_token");
+      return NextResponse.redirect(getURL("/"));
+    }
+
+    if (res.access_token?.length)
+      cookieStore.set("access_token", res.access_token as string, {
+        expires: new Date(Date.now() + res.expires_in * 1000),
+        httpOnly: true,
+        path: "/",
+      });
+
+    if (res.refresh_token?.length)
+      cookieStore.set("refresh_token", res.refresh_token as string, {
+        httpOnly: true,
+        path: "/",
+      });
+
     const user = await userInfo(res.access_token as string);
 
     const supabase = createClient();
